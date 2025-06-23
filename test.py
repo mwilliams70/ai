f8ed9d3126e94c0a237fefa2a965a746b30bb5a0
mwilliams70 <89429317+mwilliams70@users.noreply.github.com>
2025-06-23 14:20:00 -0400

    Update test.py

--- test.py
+++ test.py
@@ -5,18 +5,32 @@
 import openai
 import os
 
+from google import genai
+from google.genai.types import Tool, GenerateContentConfig, GoogleSearch
+from pydantic import BaseModel
+
 
 def main():
-    openai.api_key = "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"
-    response = openai.completions.create(
-        model="gpt-4o",
-        prompt=[
-            {"role":"system", "content": "You are a helpful assistant"}
-        ]
+    # openai.api_key = "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"
+    # response = openai.completions.create(
+    #     model="gpt-4o",
+    #     prompt=[
+    #         {"role":"system", "content": "You are a helpful assistant"}
+    #     ]
+    # )
+    # print(response['choices'][0]['message']['content'])
+    client = genai.Client(api_key="AIzaSyD5bI0i5MlVk-3pUpR2aAoDvHvkNfi6fWg")
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="How does AI work?"
     )
-    print(response['choices'][0]['message']['content'])
+    print(response.text)
 
 if __name__ == "__main__":
     main()
+
+
+
 
 
